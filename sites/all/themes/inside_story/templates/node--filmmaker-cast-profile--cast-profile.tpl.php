61092c670c4fd1b7fb739edaabdbfe855ccd871f
Daniel Carter <daniel.carter@ddotb.com>
2011-11-19 17:11:56 -0500

    1: Styled the Filmmakers page.

--- sites/all/themes/inside_story/templates/node--filmmaker-cast-profile--cast-profile.tpl.php
+++ sites/all/themes/inside_story/templates/node--filmmaker-cast-profile--cast-profile.tpl.php
@@ -77,13 +77,13 @@
  * @see template_process()
  */
 ?>
-<div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+<div id="node-<?php print $node->nid; ?>" class="cast-member-profile <?php print $classes; ?> clearfix"<?php print $attributes; ?>>
 
     <?php print $user_picture; ?>
 
     <?php print render($title_prefix); ?>
     <?php if (!$page): ?>
-        <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
+        <h2 class="profile-name capture_it2"<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
     <?php endif; ?>
     <?php print render($title_suffix); ?>
 
@@ -93,11 +93,13 @@
         </div>
     <?php endif; ?>
 
-    <div class="cast-member content"<?php print $content_attributes; ?>>
+    <div class="content clearfix"<?php print $content_attributes; ?>>
         <?php
         // We hide the comments and links now so that we can render them later.
         hide($content['comments']);
         hide($content['links']);
+        
+        $content['role'][0]['value'] = $role[0]['value'] . ", " . $title;
         print render($content);
         
         ?>
--- sites/all/themes/inside_story/templates/node--filmmaker-cast-profile--filmmaker-profile.tpl.php
+++ sites/all/themes/inside_story/templates/node--filmmaker-cast-profile--filmmaker-profile.tpl.php
@@ -77,11 +77,11 @@
  * @see template_process()
  */
 ?>
-<div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+<div id="node-<?php print $node->nid; ?>" class="filmmaker-profile <?php print $classes; ?> clearfix"<?php print $attributes; ?>>
 
     <?php print render($title_prefix); ?>
     <?php if (!$page): ?>
-        <h2<?php print $title_attributes; ?>><?php print $title; ?></h2>
+        <h2 class="profile-name capture_it2"<?php print $title_attributes; ?>><?php print $title; ?></h2>
     <?php endif; ?>
     <?php print render($title_suffix); ?>
 
@@ -91,7 +91,7 @@
         </div>
     <?php endif; ?>
 
-    <div class="filmmaker-profile content"<?php print $content_attributes; ?>>
+    <div class="content clearfix"<?php print $content_attributes; ?>>
         <?php
         // We hide the comments and links now so that we can render them later.
         hide($content['comments']);
